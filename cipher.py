38feb4467ad95663f110117d09e958a94a7ac442
Kevin Le <Kevin.Le@fcma.com>
2023-07-26 13:27:05 -0400

    done cipher w/ passing tests

--- cipher.py
+++ cipher.py
@@ -1,1 +1,27 @@
-# add your code here
+sentence = input("Please enter a sentence: ")
+sentence = sentence.lower()
+
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+alphabet2 = alphabet * 2
+cipher = []
+
+shift = 5
+letter = 0
+
+while letter < 26:
+    replacement = alphabet2[letter + shift]
+    cipher.append(replacement)
+    letter += 1
+
+new_sent = []
+
+for char in sentence:
+    if char in alphabet2:
+        indexof = alphabet2.index(char)
+        var = cipher[indexof]
+        new_sent.append(var)
+    else:
+        new_sent.append(char)
+
+translated_sent = "".join(new_sent)
+print(f"The encrypted sentence is: {translated_sent}")
